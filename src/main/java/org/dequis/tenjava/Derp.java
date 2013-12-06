a5d6fa71f573a92938fb9a895e98968e5811615e
ten.java Plugin Contest <callmekrecklow@gmail.com>
2013-12-06 18:03:01 +0000

    Nice try ;)

--- src/main/java/org/dequis/tenjava/Derp.java
+++ src/main/java/org/dequis/tenjava/Derp.java
@@ -3,7 +3,5 @@ package org.dequis.tenjava;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Derp extends JavaPlugin {
-    public void onEnable() {
-        this.getLogger().warning("WARNING: BLATANT VIOLATION OF THE CONTEST RULES BY INCLUDING ACTUAL CODE BEFORE THE CONTEST STARTS");
-    }
+    public void onEnable() {}
 }
