d4f7a4f91958703390d120321824c12e6f951c62
dequis <dx@dxzone.com.ar>
2013-12-06 11:15:18 -0300

    i am skeleton jelly

---
+++ src/main/java/org/dequis/tenjava/Derp.java
@@ -0,0 +1,9 @@
+package org.dequis.tenjava;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Derp extends JavaPlugin {
+    public void onEnable() {
+        this.getLogger().warning("WARNING: BLATANT VIOLATION OF THE CONTEST RULES BY INCLUDING ACTUAL CODE BEFORE THE CONTEST STARTS");
+    }
+}
