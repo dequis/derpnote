86c485e056bdbbd8046d2ab04a3b119b2527fc35
dequis <dx@dxzone.com.ar>
2013-12-07 07:34:46 -0300

    Contents for the book!
    I'm not actually making this about fight club, but I like that book anyway.

--- src/main/java/org/dequis/tenjava/Derp.java
+++ src/main/java/org/dequis/tenjava/Derp.java
@@ -8,6 +8,7 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.Material;
 
 public class Derp extends JavaPlugin {
@@ -26,9 +27,20 @@ public class Derp extends JavaPlugin {
                 Location loc = ((Player) sender).getLocation();
                 World w = loc.getWorld();
                 loc.setY(loc.getY() + 64);
-                ItemStack stack = new ItemStack(Material.BOOK);
+                ItemStack stack = new ItemStack(Material.BOOK_AND_QUILL);
                 ItemMeta meat = stack.getItemMeta();
                 meat.setDisplayName("Derpbook");
+                ((BookMeta) meat).setPages(
+                    "Rules of Fight Club\n" +
+                    "1st RULE: You do not talk about FIGHT CLUB.\n" +
+                    "2nd RULE: You DO NOT talk about FIGHT CLUB.\n" +
+                    "3rd RULE: If someone says \"stop\" or goes limp, taps out the fight is over.\n" +
+                    "4th RULE: Only two guys to a fight.\n" +
+                    "5th RULE: One fight at a time.\n",
+                    "6th RULE: No shirts, no shoes.\n" +
+                    "7th RULE: Fights will go on as long as they have to.\n" +
+                    "8th RULE: If this is your first night at FIGHT CLUB, you HAVE to fight.\n"
+                );
                 stack.setItemMeta(meat);
                 w.dropItem(loc, stack);
             }
